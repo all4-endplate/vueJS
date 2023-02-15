30724b7089e84f2fcefb8e4e1e1fcc4882f77fa6
An young woo <kamilin12@naver.com>
2023-02-15 20:09:02 +0900

    0woo

---
+++ src/components/common/BBSFooter.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer style="background: #333632; height: 400px;">
+    <div>
+      <div style=" float: left; width: 25%; margin-top: 50px; margin-left: 80px; height:70%">
+        <img src="../../assets/white_logo_re.png" width="130" height="60">
+        <p style="margin-right: 100px; color: gray;">_____</p>
+        <p style="margin-right: 100px; color: gray; font-size:10pt; margin-left: 40px; height:70%">모두 행복하세요</p>
+
+      </div>
+      <div style="width:20%; color: gray; float: left; margin-top: 80px; margin-left: 30px; height:70%">
+        안녕하세요<br>
+        저희는 <br>
+        1조입니다.<br>
+        저희조의 페이지인<br>
+        End Plate<br>
+        이쁘게 봐주세요
+
+      </div>
+      <div style="width:20%; color: gray; float: left; margin-top: 80px; margin-left: 30px; height:70%">
+        팀원소개<br>
+        __________<br><br>
+        박지연<br>
+        안영우<br>
+        이진주
+
+      </div>
+      <div style="width:20%; color: gray; float: left; margin-top: 80px; height:70%">
+        <img src="../../assets/facebook.png" width="64" height="64">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <img src="../../assets/insta.png" width="64" height="64">
+      </div>
+      <div style="width:15%;; height:70%">
+        <br><br><br>
+        &nbsp;
+      </div>
+    </div>
+    <div style="width: 100%;">
+      <hr style="color:gray;">
+    </div>
+    <div>
+      <span style="color:gray">© 2023 EndPlate Co., Ltd. All rights reserved.</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style scoped>
+footer{border-top: 1px solid #333632; text-align: center; font-size: 16px; color:white; margin:100px 0 0 0;}
+</style>
